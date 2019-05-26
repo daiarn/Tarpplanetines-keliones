ee17f5986feb5fa3eb6510e815f5843f6e0c581f
Dainius Arnastauskas <dainius.arnastauskas@ktu.edu>
2019-05-26 19:55:24 +0300

    Added needed methods for reservation controller

--- app/controllers/ReservationController.java
+++ app/controllers/ReservationController.java
@@ -1,4 +1,24 @@
 package controllers;
 
-public class ReservationController {
+import models.Reservation;
+import play.mvc.Controller;
+import play.mvc.Result;
+
+import java.util.List;
+
+public class ReservationController extends Controller {
+
+    public Result listVoyage(){
+
+        List<Reservation> voyageList = Reservation.find.all();
+
+        return ok(views.html.Reservation.reservations.render(voyageList));
+    }
+
+    public Result getReservation(Integer id) {
+
+        Reservation reservation = Reservation.find.byId(id);
+
+        return ok(views.html.Reservation.reservationInfo.render(reservation));
+    }
 }
