98a12676efeaa56436e36fe4c7947e4145ddd22d
Dainius Arnastauskas <dainius.arnastauskas@ktu.edu>
2019-05-27 15:29:35 +0300

    Added needed methods to calculate voyage price

--- app/controllers/ReservationController.java
+++ app/controllers/ReservationController.java
@@ -1,12 +1,23 @@
 package controllers;
 
 import models.Reservation;
+import models.VechileSpeed;
+import play.data.DynamicForm;
+import play.data.FormFactory;
 import play.mvc.Controller;
 import play.mvc.Result;
 
+import javax.inject.Inject;
 import java.util.List;
 
 public class ReservationController extends Controller {
+
+    private final FormFactory formFactory;
+
+    @Inject
+    public ReservationController(final FormFactory formFactory) {
+        this.formFactory = formFactory;
+    }
 
     public Result listVoyage(){
 
@@ -20,5 +31,29 @@ public class ReservationController extends Controller {
         Reservation reservation = Reservation.find.byId(id);
 
         return ok(views.html.Reservation.reservationInfo.render(reservation));
+    }
+
+    public Result getReservationPrice(Integer id) {
+
+        Reservation reservation = Reservation.find.byId(id);
+        List<VechileSpeed> vechileSpeeds = VechileSpeed.find.all();
+        double price = reservation.finalPrice;
+
+        return ok(views.html.Reservation.reservationPrice.render(reservation, vechileSpeeds, price));
+    }
+    public Result calcReservationPrice(Integer id) {
+
+        Reservation reservation = Reservation.find.byId(id);
+        List<VechileSpeed> vechileSpeeds = VechileSpeed.find.all();
+
+        DynamicForm requestData = formFactory.form().bindFromRequest();
+        double distance = Double.parseDouble(requestData.get("distanceFromEarth"));
+        Integer speed = Integer.valueOf(requestData.get("speed"));
+
+        double newPrice = VoyagePriceController.calculateNewPrice(reservation, speed, distance);
+
+
+
+        return ok(views.html.Reservation.reservationPrice.render(reservation, vechileSpeeds, newPrice));
     }
 }
